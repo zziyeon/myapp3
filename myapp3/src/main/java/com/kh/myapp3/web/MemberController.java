a9875d86e092f589b893d6d88fbc9b3a897b4395
zziyeon <jiyunsara99@naver.com>
2022-09-04 23:36:27 +0900

    수정중

--- myapp3/src/main/java/com/kh/myapp3/web/MemberController.java
+++ myapp3/src/main/java/com/kh/myapp3/web/MemberController.java
@@ -2,12 +2,14 @@ package com.kh.myapp3.web;
 
 import com.kh.myapp3.domain.Member;
 import com.kh.myapp3.domain.svc.MemberSVC;
-import com.kh.myapp3.domain.svc.ProductSVC;
 import com.kh.myapp3.web.memberForm.AddForm;
+import com.kh.myapp3.web.memberForm.MemberInfoForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,4 +38,17 @@ public class MemberController {
         return "redirect:/members/"+addedMember.getMemberId();
     }
 
+    //회원 개별 조회
+    @GetMapping("/{memberID}")
+    public String findByMemberId(
+            @PathVariable("memberId") Long memberId,
+            Model model
+    ){
+        //db에서 회원 조회
+        Member findedMember = memberSVC.findById(memberId);
+
+        //Member = >
+        MemberInfoForm memberInfoForm = new MemberInfoForm();
+        memberInfoForm.setMemberId(MemberId.getMemberId());
+    }
 }
---
+++ myapp3/src/main/java/com/kh/myapp3/web/memberForm/MemberInfoForm.java
@@ -0,0 +1,8 @@
+package com.kh.myapp3.web.memberForm;
+
+public class MemberInfoForm {
+    private Long MemberId;      //회원아이디
+    private String email;           //EMAIL	VARCHAR2(40 BYTE)	Yes		2
+    private String pw;              //PW	VARCHAR2(10 BYTE)	Yes		3
+    private String nickname;
+}
